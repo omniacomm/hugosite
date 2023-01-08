d244bdd774a6143688542ebf30f5a94cde630492
Luis <lhuisy@gmail.com>
2023-01-08 15:42:40 +0100

    ver 1.0.0

---
+++ assets/js/script.js
@@ -0,0 +1,201 @@
+/* ========================================================================= */
+/*	Page Preloader
+/* ========================================================================= */
+
+$(window).on('load', function () {
+	$('.preloader').fadeOut(100);
+});
+
+jQuery(function ($) {
+	"use strict";
+
+	/* ========================================================================= */
+	/*	lazy load initialize
+	/* ========================================================================= */
+
+	const observer = lozad(); // lazy loads elements with default selector as ".lozad"
+	observer.observe();
+
+	/* ========================================================================= */
+	/*	Magnific popup
+	/* =========================================================================  */
+	$('.image-popup').magnificPopup({
+		type: 'image',
+		removalDelay: 160, //delay removal by X to allow out-animation
+		callbacks: {
+			beforeOpen: function () {
+				// just a hack that adds mfp-anim class to markup
+				this.st.image.markup = this.st.image.markup.replace('mfp-figure', 'mfp-figure mfp-with-anim');
+				this.st.mainClass = this.st.el.attr('data-effect');
+			}
+		},
+		closeOnContentClick: true,
+		midClick: true,
+		fixedContentPos: false,
+		fixedBgPos: true
+	});
+
+	/* ========================================================================= */
+	/*	Portfolio Filtering Hook
+	/* =========================================================================  */
+
+	var containerEl = document.querySelector('.shuffle-wrapper');
+	if (containerEl) {
+		var Shuffle = window.Shuffle;
+		var myShuffle = new Shuffle(document.querySelector('.shuffle-wrapper'), {
+			itemSelector: '.shuffle-item',
+			buffer: 1
+		});
+
+		jQuery('input[name="shuffle-filter"]').on('change', function (evt) {
+			var input = evt.currentTarget;
+			if (input.checked) {
+				myShuffle.filter(input.value);
+			}
+		});
+	}
+
+	/* ========================================================================= */
+	/*	Testimonial Carousel
+	/* =========================================================================  */
+
+	$("#testimonials").slick({
+		infinite: true,
+		arrows: false,
+		autoplay: true,
+		autoplaySpeed: 4000
+	});
+
+	/* ========================================================================= */
+	/*	animation scroll js
+	/* ========================================================================= */
+
+
+
+	function myFunction(x) {
+		if (x.matches) {
+			var topOf = 50
+		} else {
+			var topOf = 350
+		}
+	}
+
+	var html_body = $('html, body');
+	$('nav a, .page-scroll').on('click', function () { //use page-scroll class in any HTML tag for scrolling
+		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				html_body.animate({
+					scrollTop: target.offset().top - 50
+				}, 1500, 'easeInOutExpo');
+				return false;
+			}
+		}
+	});
+
+	// easeInOutExpo Declaration
+	jQuery.extend(jQuery.easing, {
+		easeInOutExpo: function (x, t, b, c, d) {
+			if (t === 0) {
+				return b;
+			}
+			if (t === d) {
+				return b + c;
+			}
+			if ((t /= d / 2) < 1) {
+				return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+			}
+			return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+		}
+	});
+
+	/* ========================================================================= */
+	/*	counter up
+	/* ========================================================================= */
+	function counter() {
+		var oTop;
+		if ($('.count').length !== 0) {
+			oTop = $('.count').offset().top - window.innerHeight;
+		}
+		if ($(window).scrollTop() > oTop) {
+			$('.count').each(function () {
+				var $this = $(this),
+					countTo = $this.attr('data-count');
+				$({
+					countNum: $this.text()
+				}).animate({
+					countNum: countTo
+				}, {
+					duration: 1000,
+					easing: 'swing',
+					step: function () {
+						$this.text(Math.floor(this.countNum));
+					},
+					complete: function () {
+						$this.text(this.countNum);
+					}
+				});
+			});
+		}
+	}
+	$(window).on('scroll', function () {
+		counter();
+	});
+
+});
+
+
+
+// Get all sections that have an ID defined
+const sections = document.querySelectorAll("section[id]");
+console.log("SEEECTION!!" + sections[0].id);
+
+
+const links = document.querySelectorAll('[id="select-language"]');
+console.log(links);
+
+targetLang = document.querySelector('[selected]');
+console.log(targetLang);
+
+// Add an event listener listening for scroll
+window.addEventListener("scroll", navHighlighter);
+
+function navHighlighter() {
+  
+  // Get current scroll position
+  let scrollY = window.pageYOffset;
+  
+  // Now we loop through sections to get height, top and ID values for each
+  sections.forEach(current => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+    
+    /*
+    - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+    - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+    */
+    if (
+      scrollY > sectionTop &&
+      scrollY <= sectionTop + sectionHeight
+    ){
+      console.log(current.id);
+		
+	  for(i=0 ; i< links[0].length ; i++)
+	  {
+		//if(link != targetLang)
+			link = links[0][i];
+			if( link != targetLang)
+			{				
+				url = link.value.substring(0, link.value.lastIndexOf("/")+1) + "#" + current.id;
+				link.value = url;
+			};
+		}
+
+	  // document.querySelector(".nav-link a[href*=" + sectionId + "]").classList.add("active");
+    } else {
+      //document.querySelector(".navigation a[href*=" + sectionId + "]").classList.remove("active");
+    }
+  });
+}
